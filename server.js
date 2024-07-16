2c75375e729b8c8f25cf702957f0f13550e96ce0
cbbor <c.b.borisova@gmail.com>
2024-07-16 14:55:27 -0500

    fetch posts

--- server.js
+++ server.js
@@ -26,13 +26,20 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use("/", express.static("resources"));
 
-const posts = [[1, 10, "leah is annoying", ["no way! leah is my fav"], 4, "cathborisova"], 
-	[2, 25, "kaylor needs to break up with aaron already", ["yes omg he is so annoying", "he is such a manipulator"], 5, "cathy"],
-	[3, 1, "what is love island?", [], 0, "anonymous"]]; // [id, episode, text, [comments], likes, username]
 app.get("/", async(req, res) => {
 	res.set('Content-Type', 'text/html');
-	res.status(200);
-	res.render("index.pug", {episodes: 28, posts: posts});
+		
+		try {
+			const querySnapshot = await getDocs(collection(db, '/posts'));
+			const data = [];
+			querySnapshot.forEach((doc) => {
+			data.push(doc.data());
+			});
+			res.render("index.pug", {episodes: 28, posts: data});
+			res.status(200);
+		} catch (error) {
+			res.status(500).send('Error getting data from Firestore: ' + error.message);
+		}
 });
 
 app.use((req, res) => {
@@ -41,18 +48,6 @@ app.use((req, res) => {
 	res.send("Error");
 });
 
-app.get('/allposts', async (req, res) => {
-	try {
-		const querySnapshot = await getDocs(collection(db, 'posts'));
-		const data = [];
-		querySnapshot.forEach((doc) => {
-		  data.push(doc.data());
-		});
-		res.json(data);
-	  } catch (error) {
-		res.status(500).send('Error getting data from Firestore: ' + error.message);
-	  }
-});
 
 app.listen(3000, ()=>{
 	console.log("Started");
